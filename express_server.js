a49fdb0a209838189bc8f924f7136f6c62d7fcb4
Anneloes <anneloes9405@hotmail.com>
2020-01-21 01:12:13 +0000

    made a new URL file that should take in long URL and POST

--- express_server.js
+++ express_server.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let randomKey = ""
+    for (let i = 1; i <= 6; i++) {
+      
+    }
+}
+
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,10 +31,31 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/urls", (req, res) => {
-  let urlVars = { urls: urlDatabase };
-  res.render("urls_index", urlVars);
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+
+  // receive longURL -->
+  //  generate random key
+  // store in DB using ^^^ : longURL
+  // go to the shortUrl's page (/urls/jjkdjk)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
