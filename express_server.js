fc2a6e2c7a63f592fb172810c417892028d80c30
Anneloes <anneloes9405@hotmail.com>
2020-01-21 02:01:17 +0000

    work in progress, /u/jdijck won't redirect to longURL yet

--- express_server.js
+++ express_server.js
@@ -40,8 +40,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  let randomKey = generateRandomString()
+  console.log(req)
+  urlDatabase[randomKey] = req.body.longURL
+  res.redirect(`/urls/${randomKey}`)
 
   // receive longURL -->
   //  generate random key
@@ -51,6 +53,11 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[shortURL]
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
